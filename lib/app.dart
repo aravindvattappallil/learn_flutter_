e120e4f9fb4d89c2b0e6760301672d78c4dca892
Aravind Vattappallil <aravindvr99@gmail.com>
2021-04-24 01:26:16 +0530

    learned about container ,its parameter,decorations etc

--- lib/app.dart
+++ lib/app.dart
@@ -6,12 +6,24 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: "First App",
       home: Scaffold(
-        body: Container(
+          body: Center(
+        child: Container(
+          width: 400,
+          height: 400,
+          margin: EdgeInsets.all(10),
+          padding: EdgeInsets.all(40),
+          decoration: BoxDecoration(
+              color: Colors.red,
+              border: Border.all(
+                color: Colors.blue,
+                width: 5,
+              ),
+              borderRadius: BorderRadius.circular(20)),
           child: Center(
             child: Text("first app"),
           ),
         ),
-      ),
+      )),
     );
   }
 }
