e7cc0084d50293843e7aab361d30984279d27a3a
Aravind Vattappallil <aravindvr99@gmail.com>
2021-05-03 01:08:07 +0530

    basic named parameters in scaffold and appbar ,drawer floating action button and listtile

--- lib/app.dart
+++ lib/app.dart
@@ -15,6 +15,87 @@ class App extends StatelessWidget {
       title: "First App",
       home: Scaffold(
         body: Buttonwidget(),
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          elevation: 100,
+          centerTitle: true,
+          title: Text("hello!!!"),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  print("icon 1");
+                },
+                icon: Icon(Icons.notifications)),
+            IconButton(
+                onPressed: () {
+                  print("icon 2");
+                },
+                icon: Icon(Icons.favorite)),
+            IconButton(
+                onPressed: () {
+                  print("icon 3");
+                },
+                icon: Icon(Icons.alarm))
+          ],
+        ),
+        floatingActionButton: FlatButton(
+          onPressed: () {
+            print("floating pressed");
+          },
+          child: Icon(Icons.add),
+          color: Colors.red,
+          shape: CircleBorder(),
+          padding: EdgeInsets.all(10),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              DrawerHeader(
+                child: Text("home"),
+                decoration: BoxDecoration(
+                  color: Colors.greenAccent,
+                ),
+              ),
+              ListTile(
+                title: Text("favourites"),
+                subtitle: Text("subtitle"),
+                leading: Icon(Icons.favorite),
+                trailing: Icon(Icons.arrow_forward),
+                onTap: () {
+                  print("list tile 1 pressedd");
+                },
+                onLongPress: () {
+                  print("long press listtile");
+                },
+              ),
+              ListTile(
+                title: Text("setting"),
+                subtitle: Text("click to go to setting"),
+                leading: Icon(Icons.settings),
+                trailing: Icon(Icons.arrow_forward),
+                onTap: () {
+                  print("call aravind");
+                },
+                onLongPress: () {
+                  print("more option");
+                },
+              ),
+              ListTile(
+                title: Text("Account"),
+                subtitle: Text("click to navigate to account"),
+                leading: Icon(Icons.account_box),
+                trailing: Icon(Icons.call),
+                onTap: () {
+                  print("call aravind");
+                },
+                onLongPress: () {
+                  print("more option");
+                },
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
